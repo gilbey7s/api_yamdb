ea5f3a5c5388d2487cff6ddadbe1ca191ca7f863
Максим Кудряшов <dunor@mail.ru>
2022-01-07 15:32:33 +0700

    added models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.DateField()
+    description = models.TextField()
+    genre = ''
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+
+class GenreTitle(models.Model):
+    pass
