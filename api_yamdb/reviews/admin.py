99879530e289044b181f484e1fb5facba0476e7f
gilbey7s <s.l.a.y.e.r.energy@yandex.ru>
2022-01-11 22:32:42 +0300

    убрал конфликты

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,18 +1,12 @@
 from django.contrib import admin
-<<<<<<< HEAD
 
-from .models import Title, Genre, Category, GenreTitle
+from .models import Title, Genre, Category, GenreTitle, CustomUser
 
 admin.site.register(Genre)
 admin.site.register(Category)
 admin.site.register(Title)
 admin.site.register(GenreTitle)
-=======
-from .models import CustomUser
-
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     pass
-    
->>>>>>> feature/users
